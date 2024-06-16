32b60093cbc088a13aa6474311487888029b3558
veniharuka <kurobas1384@gmail.com>
2024-06-16 10:18:44 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int answer1 = Integer.parseInt(n_str);
+        answer=String.valueOf(answer1);
+        return answer;
+    }
+}
