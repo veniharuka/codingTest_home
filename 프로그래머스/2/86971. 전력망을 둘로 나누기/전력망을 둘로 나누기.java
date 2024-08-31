e2785a6a51dbab4dae0ec92bfd2909986696b7af
veniharuka <kurobas1384@gmail.com>
2024-09-01 08:39:32 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.59 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    int answer;
+    public int solution(int n, int[][] wires) {
+        answer = n;
+        
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+        for (int[] wire : wires) {
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
+        }
+        
+       
+        boolean[] visited = new boolean[n+1];
+        dfs(graph, visited, 1, n);
+        
+        return answer;
+    }
+    
+    int dfs(Map<Integer, List<Integer>> graph, boolean[] visited, int cur, int n) {
+        int count = 1;
+        visited[cur] = true;
+        
+        for (int next : graph.get(cur)) {
+            if (!visited[next]) {
+                count += dfs(graph, visited, next, n);
+            }
+        }
+      	
+        answer = Math.min(answer, Math.abs(n - count * 2));
+        
+     
+        return count;
+    }
+}
