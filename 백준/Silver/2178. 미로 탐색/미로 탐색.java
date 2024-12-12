ae4979e4f9313daae20ceb2cf3bbaf3d765e04a3
veniharuka <kurobas1384@gmail.com>
2024-12-13 06:43:47 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 16364 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,55 +2,56 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m;
+    static int n,m;
     static int[][] graph;
     static boolean[][] visited;
-    static int[] dx = {0, 1, 0, -1}; 
-    static int[] dy = {1, 0, -1, 0};
-
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        m = Integer.parseInt(input[1]);
-
-        graph = new int[n][m];
-        visited = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            String[] line = br.readLine().split("");
-            for (int j = 0; j < m; j++) {
-                graph[i][j] = Integer.parseInt(line[j]);
+    static int[] dx = {0, 1, 0 ,-1};
+    static int[] dy = {1, 0, -1 ,0};
+        
+        public static void main(String args[]) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+            String[] input = br.readLine().split(" ");
+            n = Integer.parseInt(input[0]);
+            m = Integer.parseInt(input[1]);
+            
+            graph = new int[n][m];
+            visited = new boolean[n][m];
+            
+            for(int i=0; i<n; i++){
+                String[] line = br.readLine().split("");
+                for(int j=0; j<m; j++){
+                    graph[i][j] = Integer.parseInt(line[j]);
+                }
             }
+            System.out.println(bfs(0,0));
         }
-
-        System.out.println(bfs(0, 0)); 
-    }
+    
 
     static int bfs(int x, int y) {
         Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[] {x, y});
-        visited[x][y] = true;
-
-        while (!q.isEmpty()) {
-            int[] current = q.poll();
-            int cx = current[0];
-            int cy = current[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (graph[nx][ny] == 1 && !visited[nx][ny]) {
+        q.add(new int[] {x,y});
+        visited[x][y]= true;
+        
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            
+            for(int i=0; i<4; i++){
+                int nx = cx+dx[i];
+                int ny = cy+dy[i];
+                
+                if(nx>=0 && ny>=0 && nx<n && ny<m){
+                    if(graph[nx][ny]==1 && !visited[nx][ny]){
                         visited[nx][ny] = true;
-                        graph[nx][ny] = graph[cx][cy] + 1; 
-                        q.add(new int[] {nx, ny});
+                        graph[nx][ny] = graph[cx][cy]+1;
+                        q.add(new int[] {nx,ny});
                     }
                 }
             }
         }
-
-        return graph[n - 1][m - 1]; 
+        return graph[n-1][m-1];
+        
     }
 }
