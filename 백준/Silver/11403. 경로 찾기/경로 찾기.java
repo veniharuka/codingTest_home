ef3d5f34b5db00b8926c18512976df48ab90c8e2
veniharuka <kurobas1384@gmail.com>
2025-02-17 13:00:00 +0900

    [Silver I] Title: 경로 찾기, Time: 304 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int[][] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) { 
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) { 
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1; 
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
