7ef6b011c295d69a436e703547ecf1a0aca4d281
veniharuka <kurobas1384@gmail.com>
2025-05-31 01:23:43 +0900

    [Silver V] Title: 뒤집기, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int count0 = 0;
+        int count1 = 0; 
+
+        if (s.charAt(0) == '0') {
+            count0 = 1;
+        } else {
+            count1 = 1;
+        }
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(i-1)) {
+                if (s.charAt(i) == '0') {
+                    count0++;
+                } else {
+                    count1++;
+                }
+            }
+        }
+
+        System.out.println(Math.min(count0, count1));
+    }
+}
