55543e470405f162f6e27bc0408a811704136308
veniharuka <kurobas1384@gmail.com>
2025-06-10 04:13:48 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int result = 0;
+        
+        for (int i = 1; i < N; i++) {
+            int sum = i;
+            int temp = i;
+            
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            
+            if (sum == N) {
+                result = i;
+                break;
+            }
+        }
+        
+        System.out.println(result);
+    }
+}
