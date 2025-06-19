242e5d8b2ef665ae73678f27617a19dc9b263134
veniharuka <kurobas1384@gmail.com>
2025-06-20 00:12:44 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        boolean[][] paper = new boolean[100][100];
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    paper[j][k] = true;
+                }
+            }
+        }
+        
+        int area = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (paper[i][j]) {
+                    area++;
+                }
+            }
+        }
+        
+        System.out.println(area);
+    }
+}
